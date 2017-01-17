676523b0657f25d833d3ebf0311dbf721f265222
vindictusinn <saniteeandduros@gmail.com>
2017-01-17 12:22:33 -0800

    Try this

--- js/builder/kiki.js
+++ js/builder/kiki.js
@@ -1,10 +1,25 @@
 change_weapon = function() {
 	kiki_weapon_stats = $('#select_gear :selected').val();
+	grade = $('#select_grade :selected').val();
 	if (kiki_weapon_stats == 'silver_fox_bead') {
-		weapon_att = 24976
-		weapon_att_spd = 4.6
-		weapon_crit = 12
-		weapon_crit_dmg = 44.3
+		if (grade == 'epic') {
+			weapon_att = 5968
+			weapon_att_spd = 3.3
+			weapon_crit = 7.8
+			weapon_crit_dmg = 36.2
+		}
+		if (grade == 'legendary') {
+			weapon_att = 12304
+			weapon_att_spd = 3.9
+			weapon_crit = 9.8
+			weapon_crit_dmg = 40
+		}
+		if (grade == 'immortal') {
+			weapon_att = 24976
+			weapon_att_spd = 4.6
+			weapon_crit = 12
+			weapon_crit_dmg = 44.3
+		}
 	}
 	if (kiki_weapon_stats == 'glorious_wreath') {
 		weapon_att = 27469
@@ -18,20 +33,28 @@ change_weapon = function() {
 		weapon_crit = 12
 		weapon_crit_dmg = 34.5
 	}
-	
 	$('#attack').html(weapon_att)
+	$('#att_spd').html(weapon_att_spd)
+	$('#crit').html(weapon_crit)
+	$('#crit_dmg').html(weapon_crit_dmg)
+}
+
+change_grade = function(){
+	$('#select_grade').css('display','block')
+	$('#select_grade').change(change_weapon)
 }
 
 kiki_weapon = function() {
 	$('#part_title').html('Weapon')
-	$('#select_gear').css('display','block')
 	console.log('Pick your weapon.')
 	weapon_pieces = '<option value="empty">Select weapon</option>'+
 		'<option value="silver_fox_bead">Silver Fox Bead</option>'+
 		'<option value="glorious_wreath">Glorious Wreath</option>'+
 		'<option value="disciplinary_thorn">Disciplinary Thorn</option>'
 	$('#select_gear').html(weapon_pieces)
-	$('#select_gear').change(change_weapon)
+	$('#select_gear').change(change_grade)
+	
+	$('#select_gear').css('display','block')
 }
 
 kiki_spec = function() {
