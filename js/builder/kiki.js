9900ab238dbd69ca3ebd1721e3b3a2fba5faab1b
vindictusinn <saniteeandduros@gmail.com>
2017-01-17 11:34:13 -0800

    Adding right names

--- js/builder/kiki.js
+++ js/builder/kiki.js
@@ -1,18 +1,18 @@
-change_helm = function() {
-	kiki_helm_stats = $('#select_gear :selected').val();
-	if (kiki_helm_stats == 'silver_fox_bead') {
+change_weapon = function() {
+	kiki_weapon_stats = $('#select_gear :selected').val();
+	if (kiki_weapon_stats == 'silver_fox_bead') {
 		weapon_att = 24976
 		weapon_att_spd = 4.6
 		weapon_crit = 12
 		weapon_crit_dmg = 44.3
 	}
-	if (kiki_helm_stats == 'glorious_wreath') {
+	if (kiki_weapon_stats == 'glorious_wreath') {
 		weapon_att = 27469
 		weapon_att_spd = 4.6
 		weapon_crit = 8
 		weapon_crit_dmg = 44.3
 	}
-	if (kiki_helm_stats == 'disciplinary_thorn') {
+	if (kiki_weapon_stats == 'disciplinary_thorn') {
 		weapon_att = 27469
 		weapon_att_spd = 4.6
 		weapon_crit = 12
@@ -22,21 +22,21 @@ change_helm = function() {
 	$('#attack').html(weapon_att)
 }
 
-kiki_helm = function() {
-	$('#part_title').html('Helm')
+kiki_weapon = function() {
+	$('#part_title').html('Weapon')
 	$('#select_gear').css('display','block')
 	console.log('Pick your weapon.')
-	helm_pieces = '<option value="empty">Select Helm</option>'+
+	weapon_pieces = '<option value="empty">Select weapon</option>'+
 		'<option value="silver_fox_bead">Silver Fox Bead</option>'+
 		'<option value="glorious_wreath">Glorious Wreath</option>'+
 		'<option value="disciplinary_thorn">Disciplinary Thorn</option>'
-	$('#select_gear').html(helm_pieces)
-	$('#select_gear').change(change_helm)
+	$('#select_gear').html(weapon_pieces)
+	$('#select_gear').change(change_weapon)
 }
 
 kiki_spec = function() {
 	console.log('Kiki has been summoned.')
-	$('#helm').click(kiki_helm)
+	$('#weapon').click(kiki_weapon)
 }
 
 $(document).ready(kiki_spec)
