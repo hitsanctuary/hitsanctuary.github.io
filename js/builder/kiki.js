d02760e54f1f28d7746831e7de9587b157173a02
vindictusinn <saniteeandduros@gmail.com>
2017-01-17 11:28:30 -0800

    load up this one

--- js/builder/kiki.js
+++ js/builder/kiki.js
@@ -1,16 +1,25 @@
-gear_loadout = function() {
+change_helm = function() {
+	$('#select_gear :selected').val();
+	if (Kiki_Weapon == 'silver_fox_bead') {
+		weapon_att = 24976
+		weapon_att_spd = 4.6
+		weapon_crit = 12
+		weapon_crit_dmg = 44.3
+	}
 	
+	$('#attack').html(weapon_att)
 }
 
 kiki_helm = function() {
 	$('#part_title').html('Helm')
-	$('#select_gear').attr('value','helm')
+	$('#select_gear').css('display','block')
+	console.log('Pick your weapon.')
 	helm_pieces = '<option value="empty">Select Helm</option>'+
 		'<option value="silver_fox_bead">Silver Fox Bead</option>'+
 		'<option value="glorious_wreath">Glorious Wreath</option>'+
 		'<option value="disciplinary_thorn">Disciplinary Thorn</option>'
 	$('#select_gear').html(helm_pieces)
-	gear_loadout()
+	$('#select_gear :selected').change(change_helm)
 }
 
 kiki_spec = function() {
